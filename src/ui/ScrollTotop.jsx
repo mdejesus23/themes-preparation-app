ff5eb47daa05fdaec35bf79174d07a74bfae51d3
melnard <dejesusmelnard@gmail.com>
2025-05-12 18:41:48 +0800

    revise scroll to top button

--- src/ui/ScrollTotop.jsx
+++ src/ui/ScrollTotop.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { HiMiniChevronDoubleUp } from 'react-icons/hi2';
 
 const ScrollToTopButton = () => {
   const [visible, setVisible] = useState(false);
@@ -23,10 +24,10 @@ const ScrollToTopButton = () => {
     visible && (
       <button
         onClick={scrollToTop}
-        className="fixed bottom-20 right-4 z-10 flex cursor-pointer items-center justify-center bg-yellow p-2 text-sm font-bold text-[#164B78] md:bottom-10 md:right-10 md:p-3"
+        className="fixed bottom-20 right-4 z-10 flex cursor-pointer items-center justify-center bg-yellow p-2 text-sm font-bold text-neutral-700 md:bottom-10 md:right-10 md:p-3"
         aria-label="Scroll to top"
       >
-        ↑
+        <HiMiniChevronDoubleUp />
       </button>
     )
   );
