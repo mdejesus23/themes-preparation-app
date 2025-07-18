04a1f02ce4e4005c667f8503459ce8eb8701c829
melnard <dejesusmelnard@gmail.com>
2025-07-18 09:35:53 +0800

    chore: revise heading and footer

--- src/pages/Songs.jsx
+++ src/pages/Songs.jsx
@@ -4,7 +4,7 @@ function Songs() {
   return (
     <>
       <h1 className="mb-6 font-headfont text-3xl font-bold md:text-4xl">
-        He Rose From Death
+        Psalms
       </h1>
       <SongBook />
     </>
